92185233be0ae834175205ac3df01b57c11c1092
damiel-hub <damiel8559@gmail.com>
2025-12-01 21:03:54 +0800

    Update check_gmsh.m to include mesh plotting functionality and add Watlab workflow presentation

--- check_gmsh.m
+++ check_gmsh.m
@@ -1,5 +1,10 @@
+addpath(genpath('functions'))
+
 dem_path = 'data/raster/raw/laonongDEM_5m.tif';
 gmsh_path = 'data/msh/laonong_narrow_50m/gmsh.msh';
+plt_mesh = 1;
+
+[xMesh_dem, yMesh_dem, zMesh_dem] = readGeoTiff(dem_path);
 
 figure
 lightterrain2D_imagesc(xMesh_dem, yMesh_dem, zMesh_dem)
